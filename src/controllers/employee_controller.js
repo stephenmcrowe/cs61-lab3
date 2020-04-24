44da8a7ec0751d94e63513dfce65d633f45ea7d7
Stephen Crowe <crowe.stephen.m@gmail.com>
2020-04-24 16:50:46 -0700

    Created full basic CRUD capabilities

--- src/controllers/employee_controller.js
+++ src/controllers/employee_controller.js
@@ -1,40 +1,127 @@
 import bcrypt from 'bcryptjs';
 import mysql from 'mysql';
-import connection from '../db';
-import { createMySqlDate, formatResult } from '../utils/format';
+import db from '../db';
+import createMySqlDate from '../utils/format';
 
+const INSERT = 'INSERT INTO Employees SET ?';
+const SELECT_NEW_UUID = 'SELECT UUID()';
 export const createEmployee = (req, res) => {
-  console.log(req.body);
-  const salt = bcrypt.genSaltSync(10);
-  const password = bcrypt.hashSync(req.body.Password, salt);
   const hiredate = req.body.HireDate ? new Date(req.body.HireDate) : new Date();
-  const em = {
-    EmployeeId: mysql.raw('UUID_TO_BIN(UUID(), true)'),
+  const result = {
     Username: req.body.Username,
-    Psswrd: password,
     HireDate: createMySqlDate(hiredate),
     Salary: req.body.Salary,
-    IsAdmin: req.body.IsAdmin,
-    Salt: salt,
+    IsAdmin: req.body.IsAdmin ? req.body.IsAdmin : 0, // Default to 0
   };
-  console.log(em);
-  connection.query('INSERT INTO Employees SET ?', em, formatResult(res));
+  let uuid = '';
+  db.query(SELECT_NEW_UUID)
+    .then((uuidResults) => {
+      uuid = uuidResults[0]['UUID()'];
+      const salt = bcrypt.genSaltSync(10);
+      const password = bcrypt.hashSync(req.body.Password, salt);
+      const em = Object.assign({
+        EmployeeId: mysql.raw(`UUID_TO_BIN('${uuid}', true)`),
+        Psswrd: password,
+        Salt: salt,
+      }, result);
+      result.EmployeeId = uuid;
+      return db.query(INSERT, em);
+    })
+    .then(() => {
+      res.json({ status: 200, error: null, response: result });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ status: 500, error: err.sqlMessage, response: null });
+    });
 };
 
+const SELECT_ALL = `
+SELECT BIN_TO_UUID(EmployeeId, true) AS EmployeeID, 
+Username, HireDate, Salary, IsAdmin 
+FROM Employees
+`;
+const SELECT_WHERE = `${SELECT_ALL}
+WHERE ?`;
 export const getEmployees = (req, res) => {
   console.log(req.query);
-  let query = [];
-  Object.entries(req.query).forEach(([k, v]) => {
-    query.push(`${k} = '${v}'`);
-  });
-  console.log(query);
-  query = mysql.raw(query.join(' AND '));
-  connection.query('SELECT * FROM Employees WHERE ?', query, formatResult(res));
+  if (Object.keys(req.query).length === 0) {
+    db.query(SELECT_ALL)
+      .then((result) => {
+        res.json({ status: 200, error: null, response: result });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ status: 500, error: err.sqlMessage, response: null });
+      });
+  } else {
+    let query = [];
+    Object.entries(req.query).forEach(([k, v]) => {
+      query.push(`${k} = '${v}'`);
+    });
+    query = mysql.raw(query.join(' AND '));
+    db.query(SELECT_WHERE, query)
+      .then((result) => {
+        res.json({ status: 200, error: null, response: result });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ status: 500, error: err.sqlMessage, response: null });
+      });
+  }
 };
 
+const WHERE_ID = 'WHERE EmployeeId = UUID_TO_BIN(?, true)';
+const SELECT_BY_ID = `${SELECT_ALL} ${WHERE_ID}`;
 export const getEmployee = (req, res) => {
-  // Getting the id in an appropriate format will be annoying
-  console.log(req.params.id);
-  connection.query('SELECT * FROM Employees WHERE EmployeeId = ?',
-    req.params.id, formatResult(res));
+  db.query(SELECT_BY_ID, req.params.id)
+    .then((result) => {
+      res.json({ status: 200, error: null, response: result });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ status: 500, error: err.sqlMessage, response: null });
+    });
+};
+
+const UPDATE_BY_ID = `UPDATE Employees SET ? ${WHERE_ID}`;
+export const updateEmployee = (req, res) => {
+  const update = req.body;
+  if (req.body.Password) {
+    const Salt = bcrypt.genSaltSync(10);
+    const Psswrd = bcrypt.hashSync(req.body.Password, Salt);
+    Object.assign(update, { Salt, Psswrd });
+  }
+  if (req.body.HireDate) {
+    const hd = new Date(req.body.HireDate);
+    if (hd instanceof Date && !Number.isNaN(hd.getTime())) {
+      Object.assign(update, { HireDate: createMySqlDate(hd) });
+    } else {
+      return res.json({
+        status: 400,
+        error: 'invalid HireDate',
+        response: null,
+      });
+    }
+  }
+  db.query(UPDATE_BY_ID, [update, req.params.id])
+    .then((result) => {
+      res.json({ status: 200, error: null, response: result });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ status: 500, error: err.sqlMessage, response: null });
+    });
+};
+
+const DELETE_BY_ID = `DELETE FROM Employees ${WHERE_ID}`;
+export const deleteEmployee = (req, res) => {
+  db.query(DELETE_BY_ID, req.params.id)
+    .then((result) => {
+      res.json({ status: 200, error: null, response: result });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ status: 500, error: err.sqlMessage, response: null });
+    });
 };
--- src/utils/format.js
+++ src/utils/format.js
@@ -1,14 +1,3 @@
-export function createMySqlDate(date) {
+export default function createMySqlDate(date) {
   return date.toLocaleDateString('fr-CA');
-}
-
-export function formatResult(res) {
-  return (err, results, fields) => {
-    if (err) {
-      console.log(err);
-      res.json({ status: 500, error: err.sqlMessage, response: null });
-      return;
-    }
-    res.json({ status: 200, error: null, response: results });
-  };
 }
